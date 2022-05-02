9384dba1201c452cb5a46e904da94c0ae64c7fa5
Milena Belianova <milena.belianova@gmail.com>
2022-05-02 16:14:03 +0400

    feat: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+  const separator = options.separator || "+";
+  const additionSeparator = options.additionSeparator || "|";
+  const string = String(str);
+  let addition = options.addition;
+  const repeatTimes = options.repeatTimes;
+  const additionRepeatTimes = options.additionRepeatTimes;
+
+  let additionStr = "";
+  let repeatedStr = string;
+
+if (addition === undefined) addition = "";  
+if (typeof addition !== 'string') addition = String(addition);
+
+  console.log(addition);
+  
+  if (additionRepeatTimes) {
+    additionStr =
+      (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+  } else {
+    additionStr = addition;
+  }
+  console.log(additionStr);
+  
+
+  if (repeatTimes) {
+    repeatedStr =
+      (string + additionStr + separator).repeat(repeatTimes - 1) +
+      (string + additionStr);
+  } else {
+    repeatedStr = string + additionStr;
+  }
+
+  return repeatedStr;
 }
+
 
 module.exports = {
   repeater
