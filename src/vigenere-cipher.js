62bcb2edc5f2cc296109d2bb9a47e7309c749dc3
Milena Belianova <milena.belianova@gmail.com>
2022-05-02 19:44:24 +0400

    feat: imlemented vigenere cipher class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ const getCode = (c) => {
+  return c.toUpperCase().charCodeAt(0) - 65; //код А
+}
+
+const getCharFromCode = (code) => {
+  return String.fromCharCode(code + 65);
+}
+
+const checkParam = (p1, p2) => {
+  if (p1 === undefined || p2 === undefined) {
+    throw new Error('Incorrect arguments!');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+}
+
+class VigenereCipheringMachine {
+  constructor(value = true) {
+    this.direct = value;
+  }
+  
+  encrypt(m, k) {
+    
+    checkParam(m, k);
+    
+    let i = 0;
+    
+    let codedMess = m.split('').map(c => {      
+      const mj = getCode(c);   //0-26
+      
+      if (mj > 26 || mj < 0) return c;
+      const kj = getCode(k[i % k.length]); //код ключа
+      const cj = (mj + kj) % 26;
+      const res = getCharFromCode(cj);
+      //console.log({mj, kj, cj, res});
+      i++;
+      return res;
+    });
+    
+    return (this.direct === false) ? codedMess.reverse().join('') : codedMess.join('');
+  }
+  
+  decrypt(coded, k) {
+    checkParam(coded, k);
+    
+    let i = 0;
+    
+    let message = coded.split('').map(c => {      
+      const cj = getCode(c);   
+      
+      if (cj > 26 || cj < 0) return c;
+      const kj = getCode(k[i % k.length]); //код ключа
+      const mj = ((cj - kj) + 26) % 26; //прибавили 26, чтобы избежать отриц. значений
+      const res = getCharFromCode(mj);
+      //console.log({mj, kj, cj, res});
+      i++;
+      return res;
+    });
+    
+    return (this.direct === false) ? message.reverse().join('') : message.join('');
   }
 }
 
